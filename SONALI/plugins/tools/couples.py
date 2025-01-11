27614023ae6eadc890c5360c6586548a4842d2fa
SilkSmithavibz <Silksmithavibz@gmail.com>
2025-01-11 19:32:01 +0530

    Update couples.py

--- SONALI/plugins/tools/couples.py
+++ SONALI/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from SONALI.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="|| ˹𖤍 𝗔𝗟𝗣𝗛𝗔 𖤍˼ ||",
-            url=f"https://t.me/PURVI_SUPPORT",
+            text="⚡𝙏𝙝𝙚 𝘼𝙧𝙘𝙝𝙞𝙩𝙚𝙘𝙩⚡",
+            url=f"https://t.me/The_Architect04",
         ),
     ],
 ]

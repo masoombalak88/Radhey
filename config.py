c977043281826ff33de2b950126332d3c0321d42
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2025-06-13 00:10:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,9 +13,9 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","Bst_dr")
+OWNER_USERNAME = getenv("OWNER_USERNAME","its_vikky_yadv")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME","Ennavall_bot")
+BOT_USERNAME = getenv("BOT_USERNAME","Radhey_music_bot")
 # --------------------------------------------------------
 BOT_NAME = getenv("BOT_NAME")
 # ---------------------------------------------------------
@@ -30,7 +30,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @PURVI_HELP_BOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6573727420))
+OWNER_ID = int(getenv("OWNER_ID", 7518534563))
 
 
 ## Fill these variables if you're deploying on heroku.
